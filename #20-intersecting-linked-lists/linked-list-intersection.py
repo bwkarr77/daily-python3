1ff5c75d43bb9a58cd971dd0f6de982afd142ebf
bwkarr77 <bwkarr77@gmail.com>
2020-12-12 21:45:18 -0600

    linked-list-intersection almost done

--- #20-intersecting-linked-lists/linked-list-intersection.py
+++ #20-intersecting-linked-lists/linked-list-intersection.py
@@ -1,5 +1,5 @@
 class Node:
-    def __inite__(self, value):
+    def __init__(self, value):
         self.value = value;
         self.next = None;
 
@@ -7,3 +7,55 @@ class LinkedList:
 
     def __init__(self):
         self.head = None;
+        self.length = 0
+
+    def printList(self):
+        temp = self.head
+        while(temp):
+            print(temp.value)
+            temp = temp.next
+
+def createLinkedList(listArray):
+    linkedList = LinkedList()
+
+    linkedList.head = Node(listArray.pop(0))
+    length = 1
+    temp = linkedList.head
+    while len(listArray) > 0:
+        newNode = Node(listArray.pop(0))
+        temp.next = newNode
+        temp = temp.next
+        length += 1
+    
+    return linkedList, length
+
+
+A = [3, 7, 8, 10]
+B = [99, 1, 8, 10, 20]
+
+listA, lengthA = createLinkedList(A)
+listB, lengthB = createLinkedList(B)
+# print('lengths: ', lengthA, lengthB)
+#####
+
+def compareLists(list1, length1, list2, length2):
+    if length1 <= length2:
+        iterable = list1
+        notIterable = list2
+    else:
+        iterable = list2
+        notIterable = list1
+    
+    visitedNodes = set()
+    temp1 = iterable.head
+    while(temp1):
+        visitedNodes.add(temp1)
+        temp1 = temp1.next
+
+    temp2 = notIterable.head
+    while (temp2):
+        if temp2 in visitedNodes:
+            return temp2
+        temp2 = temp2.next
+
+compareLists(listA, lengthA, listB, lengthB)
--- #23-mxn_maze/mxn-maze.py
+++ #23-mxn_maze/mxn-maze.py
@@ -6,66 +6,38 @@ board = [[False, False, False, False],
         [False, False, False, False], 
         [True, True, True, True]]
 
-start = [3, 0]
-end = [0, 0]
-
-reached = False
-path = [0]
-
 def getWalkableNeighbors(board, row, col):
   m, n = len(board), len(board[0])
-  up = down = left = right = True
   walkable = []
+
+  # determine if neighbors are walkable (not "t")
   if (row > 0) and (board[row-1][col] == False):  # not at top of board
     walkable.append((row-1, col))
   if (row < m - 1) and (board[row+1][col] == False):  # not at bottom of board
-    down = board[row+1][col]
     walkable.append((row+1, col))
   if (col > 0) and (board[row][col-1] == False):  # not at left side
-    left = board[row][col-1]
     walkable.append((row, col-1))
   if (col < n - 1) and (board[row][col+1] == False):  # not at right side
-    right = board[row][col + 1]
-    walkable.append((row, col-1))
+    walkable.append((row, col+1))
 
-  print('walk: ', walkable)
-  
-  # result = []
-  # for each in [up, right, down, left]:
-  #   print('each', each)
-  #   if 
-
-  return [up, right, down, left]
-
-def walkable(neighbors):
-  res = []
-  for each in neighbors:
-    if each == False:
-      res.append(each)
-  return res
-
-  
-
-# print(result)
+  return walkable  
 
 def shortest_path(board, start, end):
   visited = set()  # keep track of visited spots on board
-  queue = deque([(start), 0])
-  print('init: ', queue)
+  queue = deque([(start), 0])  # add starting location, and count = 0 to queue
+
   while queue:
     coords = queue.popleft()
     count = queue.popleft()
-    # if we are at the end, return number of steps
+    # if at the end, return number of steps
     if coords == end:
       return count
-    
+
     visited.add(coords)
     neighbors = getWalkableNeighbors(board, coords[0], coords[1])
-    print('neigh: ', neighbors)
     for neighbor in neighbors:
       if neighbor not in visited:
         queue.extend((neighbor, count+1))
-    print('here: ', coords, count)
 
 
-(shortest_path(board, (3,0), (0,0)))
+print("shortest path = ", shortest_path(board, (3,0), (0,0)))
