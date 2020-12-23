f62eeb211bcc9bba0e36edc1315540bd555c4a58
bwkarr77 <bwkarr77@gmail.com>
2020-12-22 20:39:39 -0600

    fraudActivity passes longer tests

--- Hackerrank/InterviewPrepKit/frequency_queries/FraudActivity/fraudActivity.py
+++ Hackerrank/InterviewPrepKit/frequency_queries/FraudActivity/fraudActivity.py
@@ -3,24 +3,41 @@
 # compare list to 
 
 def activityNotifications(expenditure, d):
-    trailing = expenditure[0:d]
+    freq = {}
     num_alerts = 0
     n = len(expenditure)
-    valToCheck = 0
+    median = 0
     
-    for index in range(d, n):
-        sortedList = trailing.copy()
-        sortedList.sort()
-        # print(trailing, sortedList)
-        if d%2 > 0:
-            valToCheck = sortedList[d//2]
-        else:
-            valToCheck = (sortedList[d//2] + sortedList[d//2 + 1])/2
+    medVal = d/2
+    
+    def find(idx):
+        total_count = 0
+        for i in range(201):
+            # print('forLoop: ', i, total_count) 
+            if i in freq:
+                total_count = total_count + freq[i]
+            if total_count >= idx:
+                return i    
+    
+    for index in range(n-1):
+        value = expenditure[index]
         
-        if valToCheck*2 <= expenditure[index]:
-            num_alerts += 1
-        trailing.pop(0)
-        trailing.append(expenditure[index])
+        if value in freq:
+            freq[value] += 1
+        else:
+            freq[value] = 1
+        
+        if index >= (d-1):
+            # determine the median
+            if d%2 == 0:
+                median = (find(medVal) + find(medVal + 1))/2
+            else:
+                median = find(medVal)
+                
+            if expenditure[index+1] >= median*2:
+                num_alerts += 1
+            
+            freq[expenditure[(index-d)+1]] -= 1 #removes the oldest value from freq
     return(num_alerts)
 
 print(activityNotifications([2, 3, 4, 2, 3, 6, 8, 4, 5], 5))
